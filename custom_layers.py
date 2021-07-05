Fixed problems with mixed precision training, implemented several tricks to stabilize it. Implemented PPL metric. Fixed bug in fromRGB layer. Implemented fused bias and activation layer. Fixed summaries in Tensorboard. Other improvements. Refactoring

custom_layers.py
@@ -3,12 +3,12 @@ import tensorflow as tf
 from tensorflow.keras.layers import Layer, Activation
 from tensorflow.keras import mixed_precision
 
-from utils import DEFAULT_DTYPE, DEFAULT_DATA_FORMAT, NCHW_FORMAT, NHWC_FORMAT,\
+from utils import DEFAULT_DTYPE, DEFAULT_DATA_FORMAT, NCHW_FORMAT, NHWC_FORMAT, WSUM_NAME,\
     validate_data_format, HE_GAIN, DEFAULT_USE_WSCALE, DEFAULT_TRUNCATE_WEIGHTS, DEFAULT_USE_XLA
 
 # Config constants
 from utils import ACTIVATION_FUNS_DICT, FP32_ACTIVATIONS, USE_MIXED_PRECISION, USE_WSCALE, TRUNCATE_WEIGHTS,\
-    DATA_FORMAT, RANDOMIZE_NOISE, BLUR_FILTER, USE_BIAS, MBSTD_GROUP_SIZE, MBSTD_NUM_FEATURES, \
+    DATA_FORMAT, RANDOMIZE_NOISE, BLUR_FILTER, USE_BIAS, USE_XLA, MBSTD_GROUP_SIZE, MBSTD_NUM_FEATURES, \
     DEFAULT_USE_MIXED_PRECISION, DEFAULT_RANDOMIZE_NOISE, DEFAULT_BLUR_FILTER, DEFAULT_USE_BIAS,\
     DEFAULT_MBSTD_NUM_FEATURES
 
@@ -138,7 +138,8 @@ def _downscale2d(x, factor, gain=1, data_format=DEFAULT_DATA_FORMAT):
     assert isinstance(factor, int) and factor >= 1
     validate_data_format(data_format)
 
-    if factor == 2 and x.dtype == tf.float32:
+    # Note: tf.nn.depthwise_conv2d already supports fp16
+    if factor == 2:
         f = [float(np.sqrt(gain) / factor)] * factor
         return _blur2d(x, f, normalize=False, stride=factor, data_format=data_format)
 
@@ -296,6 +297,7 @@ class ScaledLinear(Layer):
 
 
 class Bias(Layer):
+
     def __init__(self, lrmul=LRMUL, dtype=DEFAULT_DTYPE, use_xla=DEFAULT_USE_XLA,
                  data_format=DEFAULT_DATA_FORMAT, scope='', name=None):
         layer_name = make_layer_name(name, scope, 'Bias')
@@ -333,7 +335,7 @@ class Bias(Layer):
             return x + self.lrmul * self.b
         else:
             # Note: keep reshaping to allow easy weights decay between cpu and gpu models
-            return x + tf.reshape(self.lrmul * self.b, self.bias_target_shape)
+            return x + self.lrmul * tf.reshape(self.b, self.bias_target_shape)
 
 
 class Blur2d(Layer):
@@ -345,6 +347,7 @@ class Blur2d(Layer):
         self.data_format = data_format
         self.filter = filter
 
+    @tf.function
     def call(self, x, *args, **kwargs):
         return blur2d(x, filter=self.filter, data_format=self.data_format)
 
@@ -476,6 +479,7 @@ class StyleMod(Layer):
         # x: [inputs, dlatents]
         style = self.apply_bias(self.fc(x[1]))
         style = tf.reshape(style, self.style_target_shape)
+        # According to the official implementation biases associated with ys are initialized to ones
         return x[0] * (style[:, 0] + 1) + style[:, 1]
 
 
@@ -638,7 +642,7 @@ class Fused_Upscale2d_ScaledConv2d(Layer):
                  use_wscale=DEFAULT_USE_WSCALE, lrmul=LRMUL, truncate_weights=DEFAULT_TRUNCATE_WEIGHTS,
                  dtype=DEFAULT_DTYPE, use_xla=DEFAULT_USE_XLA,
                  data_format=DEFAULT_DATA_FORMAT, scope='', name=None):
-        layer_name = make_layer_name(name, scope, 'Upscale2d_Conv2d')
+        layer_name = make_layer_name(name, scope, 'Fused_Upscale2d_Conv2d')
         super(Fused_Upscale2d_ScaledConv2d, self).__init__(dtype=dtype, name=layer_name)
         assert kernel_size >= 1 and kernel_size % 2 == 1
         validate_data_format(data_format)
@@ -700,7 +704,7 @@ class Fused_ScaledConv2d_Downscale2d(Layer):
                  use_wscale=DEFAULT_USE_WSCALE, lrmul=LRMUL, truncate_weights=DEFAULT_TRUNCATE_WEIGHTS,
                  use_xla=DEFAULT_USE_XLA, dtype=DEFAULT_DTYPE,
                  data_format=DEFAULT_DATA_FORMAT, scope='', name=None):
-        layer_name = make_layer_name(name, scope, 'Conv2d_Downscale2d')
+        layer_name = make_layer_name(name, scope, 'Fused_Conv2d_Downscale2d')
         super(Fused_ScaledConv2d_Downscale2d, self).__init__(dtype=dtype, name=layer_name)
         assert kernel_size >= 1 and kernel_size % 2 == 1
         validate_data_format(data_format)
@@ -747,11 +751,101 @@ class Fused_ScaledConv2d_Downscale2d(Layer):
         return tf.nn.conv2d(x, w, strides=self.strides, padding='SAME', data_format=self.data_format)
 
 
+# See https://www.tensorflow.org/probability/api_docs/python/tfp/math/clip_by_value_preserve_gradient?hl=ru
+def clip_by_value_preserve_gradient(t, clip_value_min, clip_value_max, name=None):
+    with tf.name_scope(name or 'clip_by_value_preserve_gradient'):
+        return t + tf.stop_gradient(tf.clip_by_value(t, clip_value_min, clip_value_max) - t)
+
+
+# Fused bias + activation.
+# Custom cuda implementation is faster and uses less memory than performing the operations separately.
+# Maybe XLA helps at least a bit to achieve a similar effect.
+class Fused_Bias_Act(Layer):
+
+    def __init__(self, use_bias=True, act_name=None, lrmul=LRMUL, clamp=None,
+                 dtype=DEFAULT_DTYPE, use_xla=DEFAULT_USE_XLA,
+                 data_format=DEFAULT_DATA_FORMAT, scope='', name=None):
+        layer_name = make_layer_name(name, scope, 'Fused_Bias_Act')
+        super(Fused_Bias_Act, self).__init__(dtype=dtype, name=layer_name)
+        validate_data_format(data_format)
+        self.data_format = data_format
+        self.use_bias = use_bias
+        if act_name is None:
+            # No activation
+            self.act = None
+        elif act_name in ACTIVATION_FUNS_DICT.keys():
+            self.act = ACTIVATION_FUNS_DICT[act_name]
+        else:
+            assert False, f"Activation '{act_name}' is not supported. See ACTIVATION_FUNS_DICT"
+        self.act_name = act_name
+        self.fp32_act = (act_name in FP32_ACTIVATIONS) and (self._dtype_policy.compute_dtype != 'float32')
+        self.lrmul = lrmul
+        if clamp is not None:
+            assert clamp > 0, 'Clamp should be greater than 0'
+        self.clamp = clamp
+        self.use_xla = use_xla
+        self.scope = scope
+
+    def build(self, input_shape):
+        self.is_linear_bias = len(input_shape) == 2
+
+        if self.is_linear_bias:
+            self.units = input_shape[1]
+        else:
+            if self.data_format == NCHW_FORMAT:
+                self.bias_target_shape = [1, -1, 1, 1]
+                self.units = input_shape[1]
+            elif self.data_format == NHWC_FORMAT:
+                self.bias_target_shape = [1, 1, 1, -1]
+                self.units = input_shape[-1]
+
+        if self.use_bias:
+            with tf.name_scope(self.scope):
+                self.b = self.add_weight(
+                    name=BIASES_NAME,
+                    shape=[self.units],
+                    initializer=biases_init,
+                    trainable=True
+                )
+
+    @tf.function
+    def call(self, x, *args, **kwargs):
+        if self.use_bias:
+            # Note: keep reshaping to allow easy weights decay between cpu and gpu models
+            x += self.lrmul * (self.b if self.is_linear_bias else tf.reshape(self.b, self.bias_target_shape))
+
+        if self.act is not None:
+            x = self.act(
+                tf.cast(x, tf.float32) if self.fp32_act else x
+            )
+
+        if self.clamp is not None:
+            # Note: for some reasons when training with mixed precision (all fine for fp32)
+            # Grappler optimizer raises an error for D (but not G) network (layout failed) if values all clipped this way:
+            #   "x = tf.clip_by_value(x, -self.clamp, self.clamp)" -- doesn't work correctly
+            # The solution is to transpose inputs to NHWC format, clip them and transpose back to NCHW.
+            # Problem exists at least for NCHW format, which is the one used by GPU.
+
+            # To disable layout optimization see: https://github.com/tensorflow/tensorflow/issues/36901 (answer by ninnghazad).
+            # More about graph optimization: www.tensorflow.org/guide/graph_optimization (see layout optimizer).
+            # It should be possible to disable optimization only in current place using context manager (see tf link above).
+
+            # This approach to clipping solves all issues, however, is it correct to preserve gradient for clipped values?
+            # The official implementation uses tf.clip_by_value.
+            x = clip_by_value_preserve_gradient(x, -self.clamp, self.clamp)
+
+        return x
+
+
 #----------------------------------------------------------------------------
 # Layers as functions.
 
-def layer_dtype(config, layer_type, act_name=None):
-    use_mixed_precision = config.get(USE_MIXED_PRECISION, DEFAULT_USE_MIXED_PRECISION)
+def layer_dtype(layer_type, use_fp16=None, act_name=None, config=None):
+    if use_fp16 is not None:
+        use_mixed_precision = use_fp16
+    else:
+        use_mixed_precision = config.get(USE_MIXED_PRECISION, DEFAULT_USE_MIXED_PRECISION)
+
     if use_mixed_precision:
         policy = mixed_precision.Policy('mixed_float16')
         act_dtype = 'float32' if act_name in FP32_ACTIVATIONS else policy
@@ -766,18 +860,18 @@ def layer_dtype(config, layer_type, act_name=None):
     elif layer_type == 'act':
         return act_dtype
     elif layer_type in [
-        'pixel_norm', 'instance_norm', 'blur2d', 'upscale2d', 'downscale2d', 'minibatch_stddev'
+        'pixel_norm', 'instance_norm', 'blur2d', 'upscale2d', 'downscale2d', 'minibatch_stddev', WSUM_NAME
     ]:
         return compute_dtype
     else:
         assert False, 'Unknown layer type'
 
 
-def dense_layer(x, units, gain, lrmul=LRMUL, scope='', config=None):
+def dense_layer(x, units, gain, lrmul=LRMUL, use_fp16=None, scope='', config=None):
     use_wscale = config.get(USE_WSCALE, DEFAULT_USE_WSCALE)
     truncate_weights = config.get(TRUNCATE_WEIGHTS, DEFAULT_TRUNCATE_WEIGHTS)
     data_format = config.get(DATA_FORMAT, DEFAULT_DATA_FORMAT)
-    policy = layer_dtype(config, 'dense')
+    policy = layer_dtype('dense', use_fp16=use_fp16)
     return ScaledLinear(
         units=units, gain=gain,
         use_wscale=use_wscale, lrmul=lrmul, truncate_weights=truncate_weights,
@@ -786,111 +880,130 @@ def dense_layer(x, units, gain, lrmul=LRMUL, scope='', config=None):
 
 
 def conv2d_layer(x, fmaps, kernel_size, gain, lrmul=LRMUL,
-                 fused_up=False, fused_down=False, scope='', config=None):
+                 fused_up=False, fused_down=False, use_fp16=None, scope='', config=None):
     assert not (fused_up and fused_down)
 
     use_wscale = config.get(USE_WSCALE, DEFAULT_USE_WSCALE)
     truncate_weights = config.get(TRUNCATE_WEIGHTS, DEFAULT_TRUNCATE_WEIGHTS)
+    use_xla = config.get(USE_XLA, DEFAULT_USE_XLA)
     data_format = config.get(DATA_FORMAT, DEFAULT_DATA_FORMAT)
-    policy = layer_dtype(config, 'conv2d')
+    policy = layer_dtype('conv2d', use_fp16=use_fp16)
+
+    layer_kwargs = {
+        'fmaps': fmaps,
+        'kernel_size': kernel_size,
+        'gain': gain,
+        'use_wscale': use_wscale,
+        'lrmul': lrmul,
+        'truncate_weights': truncate_weights,
+        'dtype': policy,
+        'use_xla': use_xla,
+        'data_format': data_format,
+        'scope':scope
+    }
 
     if fused_up:
-        return Fused_Upscale2d_ScaledConv2d(
-            fmaps=fmaps, kernel_size=kernel_size, gain=gain,
-            use_wscale=use_wscale, lrmul=lrmul, truncate_weights=truncate_weights,
-            dtype=policy, data_format=data_format, scope=scope
-        )(x)
+        return Fused_Upscale2d_ScaledConv2d(**layer_kwargs)(x)
     elif fused_down:
-        return Fused_ScaledConv2d_Downscale2d(
-            fmaps=fmaps, kernel_size=kernel_size, gain=gain,
-            use_wscale=use_wscale, lrmul=lrmul, truncate_weights=truncate_weights,
-            dtype=policy, data_format=data_format, scope=scope
-        )(x)
+        return Fused_ScaledConv2d_Downscale2d(**layer_kwargs)(x)
     else:
-        return ScaledConv2d(
-            fmaps=fmaps, kernel_size=kernel_size, gain=gain,
-            use_wscale=use_wscale, lrmul=lrmul, truncate_weights=truncate_weights,
-            dtype=policy, data_format=data_format, scope=scope
-        )(x)
+        return ScaledConv2d(**layer_kwargs)(x)
 
 
-def bias_layer(x, lrmul=LRMUL, scope='', config=None):
+def bias_layer(x, lrmul=LRMUL, use_fp16=None, scope='', config=None):
+    use_xla = config.get(USE_XLA, DEFAULT_USE_XLA)
     data_format = config.get(DATA_FORMAT, DEFAULT_DATA_FORMAT)
-    policy = layer_dtype(config, 'bias')
-    return Bias(lrmul=lrmul, dtype=policy, data_format=data_format, scope=scope)(x)
+    policy = layer_dtype('bias', use_fp16=use_fp16)
+    return Bias(lrmul=lrmul, dtype=policy, use_xla=use_xla, data_format=data_format, scope=scope)(x)
 
 
-def act_layer(x, act_name, scope='', config=None):
+def act_layer(x, act_name, use_fp16=None, scope='', config=None):
     if act_name in ACTIVATION_FUNS_DICT.keys():
         act = ACTIVATION_FUNS_DICT[act_name]
     else:
-        assert False, f"Activation '{act_name}' not supported. See ACTIVATION_FUNS_DICT"
-    dtype = layer_dtype(config, 'act', act_name)
+        assert False, f"Activation '{act_name}' is not supported. See ACTIVATION_FUNS_DICT"
+    dtype = layer_dtype('act', use_fp16=use_fp16, act_name=act_name)
     return Activation(act, dtype=dtype, name=scope + act_name)(x)
 
 
-def const_layer(x, channel_size, scope='', config=None):
+def fused_bias_act_layer(x, act_name, lrmul=LRMUL, clamp=None, use_fp16=None, scope='', config=None):
+    use_bias = config.get(USE_BIAS, DEFAULT_USE_BIAS)
+    use_xla = config.get(USE_XLA, DEFAULT_USE_XLA)
     data_format = config.get(DATA_FORMAT, DEFAULT_DATA_FORMAT)
-    policy = layer_dtype(config, 'const')
+    policy = layer_dtype('bias', use_fp16=use_fp16)
+    return Fused_Bias_Act(
+        use_bias=use_bias, act_name=act_name, lrmul=lrmul, clamp=clamp,
+        dtype=policy, use_xla=use_xla, data_format=data_format, scope=scope
+    )(x)
+
+
+def const_layer(x, channel_size, use_fp16=None, scope='', config=None):
+    data_format = config.get(DATA_FORMAT, DEFAULT_DATA_FORMAT)
+    policy = layer_dtype('const', use_fp16=use_fp16)
     return Const(channel_size=channel_size, dtype=policy, data_format=data_format, scope=scope)(x)
 
 
-def noise_layer(x, scope='', config=None):
+def noise_layer(x, use_fp16=None, scope='', config=None):
     randomize_noise = config.get(RANDOMIZE_NOISE, DEFAULT_RANDOMIZE_NOISE)
+    use_xla = config.get(USE_XLA, DEFAULT_USE_XLA)
     data_format = config.get(DATA_FORMAT, DEFAULT_DATA_FORMAT)
-    policy = layer_dtype(config, 'noise')
-    return Noise(randomize_noise=randomize_noise, dtype=policy, data_format=data_format, scope=scope)(x)
+    policy = layer_dtype('noise', use_fp16=use_fp16)
+    return Noise(randomize_noise=randomize_noise, dtype=policy, use_xla=use_xla, data_format=data_format, scope=scope)(x)
 
 
-def blur_layer(x, scope='', config=None):
+def blur_layer(x, use_fp16=None, scope='', config=None):
     blur_filter = config.get(BLUR_FILTER, DEFAULT_BLUR_FILTER)
     data_format = config.get(DATA_FORMAT, DEFAULT_DATA_FORMAT)
-    dtype = layer_dtype(config, 'noise')
-    return Blur2d(filter=blur_filter, dtype=dtype, scope=scope, data_format=data_format)(x) if blur_filter is not None else x
+    dtype = layer_dtype('blur2d', use_fp16=use_fp16)
+    return Blur2d(filter=blur_filter, dtype=dtype, scope=scope, data_format=data_format)(x)
 
 
-def pixel_norm_layer(x, scope='', config=None):
+def pixel_norm_layer(x, use_fp16=None, scope='', config=None):
+    use_xla = config.get(USE_XLA, DEFAULT_USE_XLA)
     data_format = config.get(DATA_FORMAT, DEFAULT_DATA_FORMAT)
-    dtype = layer_dtype(config, 'pixel_norm')
-    return PixelNorm(dtype=dtype, data_format=data_format, scope=scope)(x)
+    dtype = layer_dtype('pixel_norm', use_fp16=use_fp16)
+    return PixelNorm(dtype=dtype, use_xla=use_xla, data_format=data_format, scope=scope)(x)
 
 
-def instance_norm_layer(x, scope='', config=None):
+def instance_norm_layer(x, use_fp16=None, scope='', config=None):
+    use_xla = config.get(USE_XLA, DEFAULT_USE_XLA)
     data_format = config.get(DATA_FORMAT, DEFAULT_DATA_FORMAT)
-    dtype = layer_dtype(config, 'instance_norm')
-    return InstanceNorm(dtype=dtype, data_format=data_format, scope=scope)(x)
+    dtype = layer_dtype('instance_norm', use_fp16=use_fp16)
+    return InstanceNorm(dtype=dtype, use_xla=use_xla, data_format=data_format, scope=scope)(x)
 
 
-def style_mod_layer(x, dlatents, scope='', config=None):
+def style_mod_layer(x, dlatents, use_fp16=None, scope='', config=None):
     use_bias = config.get(USE_BIAS, DEFAULT_USE_BIAS)
     use_wscale = config.get(USE_WSCALE, DEFAULT_USE_WSCALE)
     truncate_weights = config.get(TRUNCATE_WEIGHTS, DEFAULT_TRUNCATE_WEIGHTS)
+    use_xla = config.get(USE_XLA, DEFAULT_USE_XLA)
     data_format = config.get(DATA_FORMAT, DEFAULT_DATA_FORMAT)
-    policy = layer_dtype(config, 'style_mod')
+    policy = layer_dtype('style_mod', use_fp16=use_fp16)
     return StyleMod(
         use_bias=use_bias, use_wscale=use_wscale, truncate_weights=truncate_weights,
-        dtype=policy, data_format=data_format, scope=scope
+        use_xla=use_xla, dtype=policy, data_format=data_format, scope=scope
     )([x, dlatents])
 
 
-def downscale2d_layer(x, factor, config=None):
+def downscale2d_layer(x, factor, use_fp16=None, config=None):
     data_format = config.get(DATA_FORMAT, DEFAULT_DATA_FORMAT)
-    dtype = layer_dtype(config, 'downscale2d')
+    dtype = layer_dtype('downscale2d', use_fp16=use_fp16)
     return Downscale2d(factor=factor, dtype=dtype, data_format=data_format)(x)
 
 
-def upscale2d_layer(x, factor, config=None):
+def upscale2d_layer(x, factor, use_fp16=None, config=None):
     data_format = config.get(DATA_FORMAT, DEFAULT_DATA_FORMAT)
-    dtype = layer_dtype(config, 'upscale2d')
+    dtype = layer_dtype('upscale2d', use_fp16=use_fp16)
     return Upscale2d(factor=factor, dtype=dtype, data_format=data_format)(x)
 
 
-def minibatch_stddev_layer(x, scope='', config=None):
+def minibatch_stddev_layer(x, use_fp16=None, scope='', config=None):
     group_size = config.get(MBSTD_GROUP_SIZE, 4)
     num_new_features = config.get(MBSTD_NUM_FEATURES, DEFAULT_MBSTD_NUM_FEATURES)
+    use_xla = config.get(USE_XLA, DEFAULT_USE_XLA)
     data_format = config.get(DATA_FORMAT, DEFAULT_DATA_FORMAT)
-    dtype = layer_dtype(config, 'minibatch_stddev')
+    dtype = layer_dtype('minibatch_stddev', use_fp16=use_fp16)
     return MinibatchStdDev(
         group_size=group_size, num_new_features=num_new_features,
-        dtype=dtype, data_format=data_format, scope=scope
+        dtype=dtype, use_xla=use_xla, data_format=data_format, scope=scope
     )(x)

losses.py
@@ -41,9 +41,11 @@ def G_wgan(G, D, optimizer, latents, write_summary, step, **kwargs):
     fake_images = G(latents)
     fake_scores = fp32(D(fake_images))
     loss = tf.reduce_mean(-fake_scores)
-    if write_summary:
-        with tf.name_scope('G_WGAN-GP'):
-            tf.summary.scalar('Loss', loss, step=step)
+
+    with tf.name_scope('Loss/G_WGAN'):
+        if write_summary:
+            tf.summary.scalar('TotalLoss', loss, step=step)
+
     return loss
 
 
@@ -63,8 +65,8 @@ def D_wgan(G, D, optimizer, latents, real_images, write_summary, step,
     epsilon_penalty = tf.reduce_mean(tf.square(real_scores))
     loss += wgan_epsilon * epsilon_penalty
 
-    if write_summary:
-        with tf.name_scope('D_WGAN-GP'):
+    with tf.name_scope('Loss/D_WGAN'):
+        if write_summary:
             tf.summary.scalar('FakePartLoss', fake_part_loss, step=step)
             tf.summary.scalar('RealPartLoss', real_part_loss, step=step)
             tf.summary.scalar('EpsilonPenalty', epsilon_penalty, step=step)
@@ -112,8 +114,8 @@ def D_wgan_gp(G, D, optimizer, latents, real_images, write_summary, step,
     epsilon_penalty = tf.reduce_mean(tf.square(real_scores))
     loss += wgan_epsilon * epsilon_penalty
 
-    if write_summary:
-        with tf.name_scope('D_WGAN-GP'):
+    with tf.name_scope('Loss/D_WGAN-GP'):
+        if write_summary:
             tf.summary.scalar('FakePartLoss', fake_part_loss, step=step)
             tf.summary.scalar('RealPartLoss', real_part_loss, step=step)
             tf.summary.scalar('GradsPenalty', grads_penalty, step=step)
@@ -132,9 +134,11 @@ def G_logistic_saturating(G, D, optimizer, latents, write_summary, step, **kwarg
     fake_images = G(latents)
     fake_scores = fp32(D(fake_images))
     loss = -tf.math.softplus(fake_scores) # log(1 - logistic(fake_scores))
-    if write_summary:
-        with tf.name_scope('G_logistic_saturating'):
-            tf.summary.scalar('Loss', tf_mean(loss), step=step)
+
+    with tf.name_scope('Loss/G_logistic_saturating'):
+        if write_summary:
+            tf.summary.scalar('TotalLoss', tf_mean(loss), step=step)
+
     return loss
 
 
@@ -143,9 +147,11 @@ def G_logistic_nonsaturating(G, D, optimizer, latents, write_summary, step, **kw
     fake_images = G(latents)
     fake_scores = fp32(D(fake_images))
     loss = tf.math.softplus(-fake_scores) # -log(logistic(fake_scores))
-    if write_summary:
-        with tf.name_scope('G_logistic_nonsaturating'):
-            tf.summary.scalar('Loss', tf_mean(loss), step=step)
+
+    with tf.name_scope('Loss/G_logistic_nonsaturating'):
+        if write_summary:
+            tf.summary.scalar('TotalLoss', tf_mean(loss), step=step)
+
     return loss
 
 
@@ -156,11 +162,13 @@ def D_logistic(G, D, optimizer, latents, real_images, write_summary, step, **kwa
     real_scores = fp32(D(real_images))
     loss = tf.nn.softplus(fake_scores) # -log(1 - logistic(fake_scores))
     loss += tf.nn.softplus(-real_scores) # -log(logistic(real_scores))
-    if write_summary:
-        with tf.name_scope('D_logistic'):
+
+    with tf.name_scope('Loss/D_logistic'):
+        if write_summary:
             tf.summary.scalar('FakePartLoss', tf_mean(fake_scores), step=step)
             tf.summary.scalar('RealPartLoss', tf_mean(real_scores), step=step)
             tf.summary.scalar('TotalLoss', tf_mean(loss), step=step)
+
     return loss
 
 
@@ -200,8 +208,8 @@ def D_logistic_simplegp(G, D, optimizer, latents, real_images, write_summary, st
         r2_penalty = tf_grads_reduce_fn(tf.square(fake_grads), axis=[1, 2, 3])
         loss += r2_penalty * (r2_gamma * 0.5)
 
-    if write_summary:
-        with tf.name_scope('D_logistic_simpleGP'):
+    with tf.name_scope('Loss/D_logistic_simpleGP'):
+        if write_summary:
             tf.summary.scalar('FakePartLoss', tf_mean(fake_scores), step=step)
             tf.summary.scalar('RealPartLoss', tf_mean(real_scores), step=step)
             if use_r1_penalty:

utils.py
@@ -1,4 +1,5 @@
 import os
+import json
 import logging
 from glob import glob
 import shutil
@@ -76,6 +77,12 @@ DATA_FORMAT = 'data_format'
 USE_GPU_FOR_GS = 'use_GPU_for_Gs'
 # Use mixed precision training?
 USE_MIXED_PRECISION = 'use_mixed_precision'
+# Use FP16 for the N highest resolutions, regardless of dtype.
+NUM_FP16_RESOLUTIONS = 'num_fp16_resolutions'
+# Clamp activations to avoid float16 overflow? Note: number or None to disable
+CONV_CLAMP = 'conv_clamp'
+# Fuse bias and activation?
+FUSED_BIAS_ACT = 'fused_bias_act'
 # Data type
 # Note: it is not recommended to change it to float16, just use float32 and mixed precision training if needed
 DTYPE = 'dtype'
@@ -130,12 +137,18 @@ ADAM_BETA2 = 'adam_beta2'
 RESET_OPT_STATE_FOR_NEW_LOD = 'reset_opt_state_for_new_lod'
 # Max models to keep
 MAX_MODELS_TO_KEEP = 'max_models_to_keep'
-# How often to write summaries
-SUMMARY_EVERY = 'summary_every'
-# How often to save models weights
-SAVE_MODEL_EVERY = 'save_model_every'
-# How often to save progress images
-SAVE_IMAGES_EVERY = 'save_images_every'
+# How often to write scalar summaries (measured in thousands)
+SUMMARY_SCALARS_EVERY_KIMAGES = 'summary_scalars_every_kimages'
+# How often to write histogram summaries (measured in thousands)
+SUMMARY_HISTS_EVERY_KIMAGES = 'summary_hists_every_kimages'
+# How often to save models weights (measured in thousands)
+SAVE_MODEL_EVERY_KIMAGES = 'save_model_every_kimages'
+# How often to save progress images (measured in thousands)
+SAVE_IMAGES_EVERY_KIMAGES = 'save_images_every_kimages'
+# How often to run metrics (measured in thousands)
+RUN_METRICS_EVERY_KIMAGES = 'run_metrics_every_kimages'
+# Which metrics to compute? dict of form: {metric: [options]}
+METRICS_DICT = 'metrics'
 
 
 ### ---------- Generator Mapping network ---------- ###
@@ -258,9 +271,12 @@ VALID_MAX_PNG_RES = 'valid_max_png_res'
 
 DEFAULT_STORAGE_PATH = None
 DEFAULT_MAX_MODELS_TO_KEEP = 3
-DEFAULT_SUMMARY_EVERY = 500
-DEFAULT_SAVE_MODEL_EVERY = 5000
-DEFAULT_SAVE_IMAGES_EVERY = 500
+DEFAULT_SUMMARY_SCALARS_EVERY_KIMAGES = 5
+DEFAULT_SUMMARY_HISTS_EVERY_KIMAGES = 25
+DEFAULT_SAVE_MODEL_EVERY_KIMAGES = 500
+DEFAULT_SAVE_IMAGES_EVERY_KIMAGES = 5
+DEFAULT_RUN_METRICS_EVERY_KIMAGES = 50
+DEFAULT_METRICS_DICT = {}
 DEFAULT_DATASET_MAX_CACHE_RES = -1
 DEFAULT_START_RESOLUTION = 8
 DEFAULT_TOTAL_KIMAGES = -1
@@ -286,8 +302,11 @@ DEFAULT_MAPPING_ACTIVATION = 'leaky_relu'
 DEFAULT_OVERRIDE_G_PROJECTING_GAIN = True
 DEFAULT_G_FUSED_SCALE = True
 DEFAULT_D_FUSED_SCALE = True
+DEFAULT_FUSED_BIAS_ACT = True
 DEFAULT_DTYPE = 'float32'
 DEFAULT_USE_MIXED_PRECISION = True
+DEFAULT_NUM_FP16_RESOLUTIONS = 'auto' # 4 - value used in the original implementation
+DEFAULT_CONV_CLAMP = None # 256 - value used in the official implementation
 DEFAULT_USE_XLA = True
 DEFAULT_G_ACTIVATION = 'leaky_relu'
 DEFAULT_D_ACTIVATION = 'leaky_relu'
@@ -302,7 +321,7 @@ DEFAULT_G_LEARNING_RATE = 0.001
 DEFAULT_D_LEARNING_RATE = 0.001
 DEFAULT_G_LEARNING_RATE_DICT = {}
 DEFAULT_D_LEARNING_RATE_DICT = {}
-DEFAULT_ADAM_BETA1 = 0.
+DEFAULT_ADAM_BETA1 = 0.0
 DEFAULT_ADAM_BETA2 = 0.99
 DEFAULT_RESET_OPT_STATE_FOR_NEW_LOD = True
 DEFAULT_USE_G_SMOOTHING = True
@@ -319,7 +338,7 @@ DEFAULT_SHUFFLE_DATASET = True
 DEFAULT_MIRROR_AUGMENT = True
 DEFAULT_VALID_GRID_NROWS = 5
 DEFAULT_VALID_GRID_NCOLS = 7
-DEFAULT_VALID_MIN_TARGET_SINGLE_IMAGE_SIZE = 2 ** 7 # or maybe 2**7  ?
+DEFAULT_VALID_MIN_TARGET_SINGLE_IMAGE_SIZE = 2 ** 7 # or maybe 2**7 ?
 DEFAULT_VALID_MAX_PNG_RES = 5
 
 # Note: by default Generator and Discriminator use the same values for these constants
@@ -352,13 +371,23 @@ ACTIVATION_FUNS_DICT = {
 }
 
 # Activation function which (might?) need to use float32 dtype
+# Should this activation only use fp32?
+# https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/grappler/optimizers/auto_mixed_precision_lists.h
 FP32_ACTIVATIONS = ['selu']
 
 # For jupyter notebooks
 EXAMPLE_IMAGES_DIR = 'example_images'
 
+# NCHW -> NHWC
+toNHWC_AXIS = [0, 2, 3, 1]
+# NHWC -> NCHW
+toNCHW_AXIS = [0, 3, 1, 2]
+
+LOSS_SCALE_KEY = 'loss_scale'
+
 DEBUG_MODE = 'debug_mode'
 DEFAULT_DEBUG_MODE = '0'
+
 
 def should_log_debug_info():
     return int(os.environ.get(DEBUG_MODE, DEFAULT_DEBUG_MODE)) > 0
@@ -413,6 +442,10 @@ def is_last_step(step, n_steps):
     return step == (n_steps - 1)
 
 
+def should_write_summary(summary_every: int, n_images: int, batch_size: int):
+    return (n_images // summary_every > 0 and n_images % summary_every < batch_size) or n_images == batch_size
+
+
 def level_of_details(res, resolution_log2):
     return resolution_log2 - res + 1
 
@@ -422,10 +455,16 @@ def compute_alpha(step, total_steps):
 
 
 def update_wsum_alpha(model, alpha):
-    for layer in model.layers:
-        if WSUM_NAME in layer.name:
-            layer.set_weights([np.array(alpha)])
-    return model
+    def recursive_update_wsum_alpha(model, alpha):
+        for layer in model.layers:
+            if isinstance(layer, tf.keras.Model):
+                recursive_update_wsum_alpha(layer, alpha)
+            else:
+                if WSUM_NAME in layer.name:
+                    layer.set_weights([np.array(alpha)])
+        return model
+
+    return recursive_update_wsum_alpha(model, alpha)
 
 
 def mult_by_zero(weights):
@@ -434,6 +473,29 @@ def mult_by_zero(weights):
 
 def create_model_type_key(res, mode):
     return f'{res}_{mode}'
+
+
+def get_start_fp16_resolution(num_fp16_resolutions, start_resolution_log2, target_resolution_log2):
+    # 1) 2 - 4 - start block resolution
+    # 2) 3 - 8 - default start resolution
+    # 3) 4 - 16
+    # 4) 5 - 32
+    # 5) 6 - 64
+    # 6) 7 - 128
+    # 7) 8 - 256
+    # 8) 9 - 512
+    # 9) 10 - 1024
+    if num_fp16_resolutions == 'auto':
+        """
+        # 1st value: a new init value, 2nd value: taken from the official implementation (N = 4)
+        return max(
+            min(start_resolution_log2 + 2, target_resolution_log2 - 4 + 1), start_resolution_log2
+        )
+        """
+        # Let start block resolution and two consequent ones use fp32
+        return 2 + 2
+    else:
+        return target_resolution_log2 - num_fp16_resolutions + 1
 
 
 #----------------------------------------------------------------------------
@@ -458,14 +520,6 @@ def set_optimizer_iters(optimizer, iters):
     return optimizer
 
 
-def old_maybe_scale_loss(loss, optimizer, mixed_precision: bool):
-    return optimizer.get_scaled_loss(loss) if mixed_precision else loss
-
-
-def old_maybe_unscale_grads(grads, optimizer, mixed_precision: bool):
-    return optimizer.get_unscaled_gradients(grads) if mixed_precision else grads
-
-
 def maybe_scale_loss(loss, optimizer):
     return optimizer.get_scaled_loss(loss) if optimizer.use_mixed_precision else loss
 
@@ -474,11 +528,12 @@ def maybe_unscale_grads(grads, optimizer):
     return optimizer.get_unscaled_gradients(grads) if optimizer.use_mixed_precision else grads
 
 
-def custom_unscale_grads(grads, vars, optimizer):
+def custom_unscale_grads(grads, vars, optimizer: tf.keras.mixed_precision.LossScaleOptimizer):
     # Note: works inside tf.function
     # All grads are casted to fp32
     dtype = tf.float32
-    coef = fp32(1. / optimizer._loss_scale())
+    # TODO: check if it should be optimizer.loss_scale or optimizer._loss_scale()
+    coef = fp32(1. / optimizer.loss_scale)
 
     def unscale_grad(g, v):
         return fp32(g) * coef if g is not None else (tf.zeros_like(v, dtype=dtype), v)
@@ -490,12 +545,12 @@ def custom_unscale_grads(grads, vars, optimizer):
     return grads_array.stack()
 
 
-def old_maybe_custom_unscale_grads(grads, vars, optimizer, mixed_precision: bool=None):
-    return custom_unscale_grads(grads, vars, optimizer) if mixed_precision else grads
-
-
 def maybe_custom_unscale_grads(grads, vars, optimizer):
     return custom_unscale_grads(grads, vars, optimizer) if optimizer.use_mixed_precision else grads
+
+
+def is_finite_grad(grad):
+    return tf.equal(tf.math.count_nonzero(~tf.math.is_finite(grad)), 0)
 
 
 def trace_vars(vars, title):
@@ -609,7 +664,7 @@ h5_weights_key = 'weights'
 
 def weights_to_dict(model, optimizer_call=False):
     vars = model.trainable_variables if not optimizer_call else model.weights
-    if optimizer_call:
+    if should_log_debug_info():
         print('\nSaving weights:')
         for idx, var in enumerate(vars):
             print(f'{idx}: {var.name}')
@@ -634,10 +689,12 @@ def save_weights(weights_dict, filename):
     f = h5py.File(filename, 'w')
     g = f.create_group(h5_weights_key)
 
-    for var_name in weights_dict.keys():
+    for idx, var_name in enumerate(weights_dict.keys()):
         value = weights_dict[var_name]
         shape = value.shape
         dset = g.create_dataset(name=var_name, shape=shape, dtype=value.dtype.name)
+        # TODO: for debugging, remove later
+        # print(f'{idx}) {var_name}: {value.mean():.4f}, std={value.std():.4f}')
         if not shape:
             # Scalar
             dset[()] = value
@@ -668,25 +725,22 @@ def load_weights_into_dict(var_names, filename):
 def load_weights(model, filename, optimizer_call=False):
     vars = model.trainable_variables if not optimizer_call else model.weights
     var_names = [var.name for var in vars]
-    if optimizer_call:
-        print('\nLoading weights, var names:\n', var_names)
     var_dict = load_weights_into_dict(var_names, filename)
 
     log_debug_info = should_log_debug_info()
     loaded_vars = []
-    i = 0
+    # Note: another approach is to use set_weights (load ur use existing value) for models and optimizers (maybe with deepcopy?)
     for var in vars:
         if var.name in var_dict.keys():
-            i += 1
-            #if log_debug_info:
-            if optimizer_call or True:
-                print(f'{i}: Loading {var.name}')
+            if log_debug_info:
+                print(f'Loading {var.name}')
             # Might be a Strange line for optimizer
             var.assign(var_dict[var.name])
             loaded_vars.append(var.name)
 
-    if optimizer_call or True:
-        print('\nLoaded these vars:\n', loaded_vars)
+    # TODO: for debugging, remove later
+    # print('\nOptimizer call:', optimizer_call)
+    # print('Loaded these vars:\n', loaded_vars)
 
     return model
 
@@ -696,7 +750,7 @@ def create_model_folder_path(model_name, res, stage, step=None, storage_path=DEF
     model_name - name of configuration model
     res - current resolution
     stage - one of [TRANSITION_MODE, STABILIZATION_MODE]
-    step - number of step for given resolution and stage
+    step - number of steps (or processed images) for given resolution and stage
     storage_path - optional prefix path
     """
     res_folder = f'{2**res}x{2**res}'
@@ -719,7 +773,7 @@ def save_model(model, model_name, model_type, res,
                  used as a separate dir level
     res - current log2 resolution
     stage - one of [TRANSITION_MODE, STABILIZATION_MODE]
-    step - number of step for given resolution and stage
+    step - number of steps (or processed images) for given resolution and stage
     storage_path - optional prefix path
     Note: should probably change this fun to standard way of saving model
     """
@@ -743,6 +797,41 @@ def save_model(model, model_name, model_type, res,
     save_weights(weights_dict, filepath)
 
 
+def save_optimizer_loss_scale(optimizer: tf.keras.mixed_precision.LossScaleOptimizer,
+                              model_name: str, model_type: str, res: int, stage: str,
+                              step: int, storage_path: str = DEFAULT_STORAGE_PATH):
+    """
+    optimizer - an optimizer model from which loss scale is to be saved
+    model_name - name of configuration model
+    model_type - one of [GENERATOR_NAME, DISCRIMINATOR_NAME],
+                 used as a separate dir level
+    res - current log2 resolution
+    stage - one of [TRANSITION_MODE, STABILIZATION_MODE]
+    step - number of steps (or processed images) for given resolution and stage
+    storage_path - optional prefix path
+    Note: should probably change this fun to standard way of saving model
+    """
+    model_folder_path = create_model_folder_path(
+        model_name=model_name,
+        res=res,
+        stage=stage,
+        step=step,
+        storage_path=storage_path
+    )
+    model_folder_path += OPTIMIZER_POSTFIX
+    if not os.path.exists(model_folder_path):
+        os.makedirs(model_folder_path)
+
+    # This function is only called when loss scale is dynamic
+    loss_scale = float(optimizer._loss_scale().numpy())
+    if should_log_debug_info():
+        print(f'Saved loss scale for {model_type}: {loss_scale}')
+
+    filepath = os.path.join(model_folder_path, model_type + '.json')
+    with open(filepath, 'w') as fp:
+        json.dump({LOSS_SCALE_KEY: loss_scale}, fp)
+
+
 def load_model(model, model_name, model_type, res,
                stage, step, storage_path=DEFAULT_STORAGE_PATH):
     """
@@ -751,7 +840,7 @@ def load_model(model, model_name, model_type, res,
     model_type - one of [GENERATOR_NAME, DISCRIMINATOR_NAME], used as a separate dir level
     res - current log2 resolution
     stage - one of [TRANSITION_MODE, STABILIZATION_MODE]
-    step - number of step for given resolution and stage
+    step - number of steps (or processed images) for given resolution and stage
     storage_path - optional prefix path
     Note: should probably change this fun to standard way of loading model
     """
@@ -772,6 +861,40 @@ def load_model(model, model_name, model_type, res,
     filepath = os.path.join(model_folder_path, model_type + '.h5')
     model = load_weights(model, filepath, optimizer_call=optimizer_call)
     return model
+
+
+def load_optimizer_loss_scale(model_name: str, model_type: str, res: int, stage: str,
+                              step: int, storage_path: str = DEFAULT_STORAGE_PATH):
+    """
+    optimizer - an optimizer model from which loss scale is to be saved
+    model_name - name of configuration model
+    model_type - one of [GENERATOR_NAME, DISCRIMINATOR_NAME],
+                 used as a separate dir level
+    res - current log2 resolution
+    stage - one of [TRANSITION_MODE, STABILIZATION_MODE]
+    step - number of steps (or processed images) for given resolution and stage
+    storage_path - optional prefix path
+    Note: should probably change this fun to standard way of saving model
+    """
+    model_folder_path = create_model_folder_path(
+        model_name=model_name,
+        res=res,
+        stage=stage,
+        step=step,
+        storage_path=storage_path
+    )
+    model_folder_path += OPTIMIZER_POSTFIX
+    if not os.path.exists(model_folder_path):
+        os.makedirs(model_folder_path)
+
+    filepath = os.path.join(model_folder_path, model_type +  OPTIMIZER_POSTFIX + '.json')
+    with open(filepath, 'r') as fp:
+        loss_scale = json.load(fp)[LOSS_SCALE_KEY]
+
+    if should_log_debug_info():
+        print(f'Loaded loss scale for {model_type}: {loss_scale}')
+
+    return loss_scale
 
 
 def remove_old_models(model_name, res, stage, max_models_to_keep, storage_path=DEFAULT_STORAGE_PATH):